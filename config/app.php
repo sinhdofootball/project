40732ae0fe70ea9ddf810b222e690492cc13843b
sinhnt <upworksinh@gmail.com>
2019-09-23 16:32:26 +0700

    install laravel passport

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Zizaco\Entrust\EntrustServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
 
     ],
 
